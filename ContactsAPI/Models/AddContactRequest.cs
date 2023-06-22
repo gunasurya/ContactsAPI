655e2a02886abe6a1869ae2dcb6f318a19bcb1c0
gunasurya <gunasuryagoud@gmail.com>
2023-06-22 18:14:54 +0530

    commit

---
+++ ContactsAPI/Models/AddContactRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace ContactsAPI.Models
+{
+    public class AddContactRequest
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public long PhoneNumber { get; set; }
+
+        public string Address { get; set; } = string.Empty;    
+    }
+}
--- ContactsAPI/Program.cs
+++ ContactsAPI/Program.cs
@@ -1,7 +1,11 @@
+using ContactsAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<ContactsAPIDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("ContactApiCS")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
